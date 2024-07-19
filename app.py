ebe42bbb8067d24d3bddc584801e31d3d16e0e56
93103tim <93103tim@gmail.com>
2024-07-19 19:54:42 +0800

    Update app.py
    原本的程式碼:
    from flask import Flask, request, abort
    from linebot import LineBotApi, WebhookHandler
    from linebot.exceptions import InvalidSignatureError
    from linebot.models import *
    import os
    
    app = Flask(__name__)
    
    line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
    handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
    
    
    @app.route("/callback", methods=['POST'])
    def callback():
        signature = request.headers['X-Line-Signature']
        body = request.get_data(as_text=True)
        app.logger.info("Request body: " + body)
        try:
            handler.handle(body, signature)
        except InvalidSignatureError:
            abort(400)
        return 'OK'
    
    @handler.add(MessageEvent, message=TextMessage)
    def handle_message(event):
        message = TextSendMessage(text=event.message.text)
        line_bot_api.reply_message(event.reply_token, message)
    
    import os
    if __name__ == "__main__":
        port = int(os.environ.get('PORT', 5000))
        app.run(host='0.0.0.0', port=port)

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
-
+import google.generativeai
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
+# 設置 Google Gemini API Token
+google_gemini_api_token = os.environ['GOOGLE_GEMINI_API_TOKEN']
+google_gemini_client = google_gemini.Client(api_token=google_gemini_api_token)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -23,10 +26,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    user_message = event.message.text
+    
+    # 使用 Google Gemini 模型來獲取回答
+    gemini_response = google_gemini_client.ask(user_message)
+    reply_text = gemini_response['answer']
+    
+    message = TextSendMessage(text=reply_text)
     line_bot_api.reply_message(event.reply_token, message)
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
