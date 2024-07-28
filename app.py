2d02f35c04ab0ee7a6a2feb3271dfe036147baf1
93103tim <93103tim@gmail.com>
2024-07-28 22:07:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,97 @@
-
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
 import os
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.memory import ConversationBufferMemory
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+import google.generativeai as genai
+
+def get_chatbot_chain():
+    openai_api_key = os.environ["OPENAI_API_KEY"]
+    gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+    #初始化Gemini模型
+    genai.configure(api_key=gemini_api_key)
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    
+    #載入檔案
+    loader = CSVLoader(file_path="faq.csv")
+    documents = loader.load()
+
+    #使用Faiss vectordb儲存由OpenaiEmbedding過的內容
+    vectorstore = FAISS.from_documents(documents, OpenAIEmbeddings())
+
+    #儲存記憶
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+    chain = ConversationalRetrievalChain.from_llm(llm=model,
+                            retriever=vectorstore.as_retriever(),
+                            memory=memory)
+    return chain
+
+
+#linebot part
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+import tempfile
+import datetime
+import openai
+import time
+import traceback
 
 app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
-line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+def GPT_response(text):
+    # 接收回應
+    response = get_chatbot_chain(text)
+    print("已收到回應訊息")
+    
+    return response.text
 
 
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
+
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    msg = event.message.text
+    
+    GPT_answer = GPT_response(msg)
+    print(GPT_answer)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+        
+
+@handler.add(PostbackEvent)
+def handle_message(event):
+    print(event.postback.data)
 
 import os
 if __name__ == "__main__":
